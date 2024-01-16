45453200fb8a060880e815bec63a5cb3b9ae3fff
Inf1n1ty75 <volkandu75@gmail.com>
2024-01-16 15:46:01 +0100

    added  a function that find the PATH variable and another function that create a path with av

--- main.c
+++ main.c
@@ -6,17 +6,54 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 13:47:33 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/01/15 17:31:31 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/01/16 15:44:57 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Libft/get_next_line.h"
 #include "Libft/libft.h"
 #include "ft_printf/include/ft_printf.h"
 #include "pipex.h"
-#include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
 #include <unistd.h>
+
+char	**ft_get_path(char **env)
+{
+	char	**path;
+
+	path = NULL;
+	while (*env)
+	{
+		if (ft_strncmp(*env, "PATH=", 5) == 0)
+		{
+			path = ft_split(*env + 5, ':');
+			break ;
+		}
+		env++;
+	}
+	return (path);
+}
+
+char	*ft_create_path(int ac, char *command, char **envp)
+{
+	char 	**path;
+	char	*new_path;
+
+	path = NULL;
+	path = ft_get_path(envp);
+	(void)ac;
+	while (*path)
+	{
+		new_path = *path;
+		new_path = ft_strjoin(new_path, "/");
+		new_path = ft_strjoin_and_free(new_path, command);
+		if (access(new_path, F_OK) == 0)
+			return (new_path);
+		free(new_path);
+		path++;
+	}
+	return (NULL);
+}
 
 int	main(int ac, char **av, char **envp)
 {
@@ -27,9 +64,8 @@ int	main(int ac, char **av, char **envp)
 	char	*cmd2;
 	char	**args;
 	char	**args2;
+	char	*new_path;
 
-	(void)av;
-	(void)envp;
 	(void)ac;
 	if (pipe(fd) == -1)
 	{
@@ -72,7 +108,10 @@ int	main(int ac, char **av, char **envp)
 		close(fd[0]);
 		waitpid(pid, NULL, 0);
 	}
+	new_path = ft_create_path(ac, av[1], envp);
+	ft_printf("new_path: %s\n", new_path);
 }
+
 
 // TODO: Create a function that parse the command and return path
 // TODO: Modify the child process code to use the new function
