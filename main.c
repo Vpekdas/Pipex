1efc8498910c495f92dbaa162359f3bc8e737f93
Inf1n1ty75 <volkandu75@gmail.com>
2024-01-17 14:07:56 +0100

    seems like infile and outfile are working well

--- main.c
+++ main.c
@@ -6,11 +6,12 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/15 13:47:33 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/01/16 17:25:48 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/01/17 13:50:42 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+#include <unistd.h>
 
 int	main(int ac, char **av, char **envp)
 {
@@ -21,9 +22,9 @@ int	main(int ac, char **av, char **envp)
 	char	*cmd2 = NULL;
 	char	**args = NULL;
 	char	**args2 = NULL;
+	int		infile = open("infile.txt", O_RDONLY);
+	int		outfile = open("outfile.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
 
-	if (ac < 2)
-		return (0);
 	pipe(fd);
 	if (checking_errors_pipe(fd[0]) || checking_errors_pipe(fd[1]))
 		return (1);
@@ -33,21 +34,25 @@ int	main(int ac, char **av, char **envp)
 	if (pid == 0) // child 1
 	{
 		close(fd[0]);
+		dup2(infile, STDIN_FILENO);
+		close(infile);
 		dup2(fd[1], STDOUT_FILENO);
 		close(fd[1]);
-		args = ft_split(av[1], ' ');
+		args = ft_split(av[2], ' ');
 		cmd1 = ft_create_path(ac, args[0], envp);
 		execve(cmd1, args, NULL);
 	}
 	pid2 = fork();
 	if (checking_errors_pid(pid2))
-		return (1);
+		return (2);
 	if (pid2 == 0) // child 2
 	{
 		close(fd[1]);
+		dup2(outfile, STDOUT_FILENO);
+		close(outfile);
 		dup2(fd[0], STDIN_FILENO);
 		close(fd[0]);
-		args2 = ft_split(av[2], ' ');
+		args2 = ft_split(av[3], ' ');
 		cmd2 = ft_create_path(ac, args2[0], envp);
 		execve(cmd2, args2, NULL);
 	}
@@ -55,7 +60,8 @@ int	main(int ac, char **av, char **envp)
 	{
 		close(fd[1]);
 		close(fd[0]);
-		waitpid(pid, NULL, 0);
+		close(infile);
+		close(outfile);
 	}
 }
 
