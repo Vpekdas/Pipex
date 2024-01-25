5aaed20c51f28dcd3d389306b68182adbb62a0fb
Inf1n1ty75 <volkandu75@gmail.com>
2024-01-25 16:47:06 +0100

    error msg modified

--- main_bonus.c
+++ main_bonus.c
@@ -6,10 +6,11 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/25 14:45:51 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/01/25 14:59:54 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/01/25 16:46:01 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Libft/libft.h"
 #include "pipex.h"
 
 int	main(int ac, char **av, char **envp)
@@ -22,7 +23,10 @@ int	main(int ac, char **av, char **envp)
 	if (ac > 4 && ft_strcmp(av[1], "here_doc") == 0)
 		ft_exec_here_doc(av, envp, av[ac - 1]);
 	else
-		ft_putstr_fd("Error, arguments are less than 5\n", 2);
+	{
+		ft_putstr_fd("Error, ", 2);
+		ft_putstr_fd("expected format : here_doc LIMITER cmd cmd1 file\n", 2);
+	}
 	while (wait(NULL) > 0)
 		;
 	return (0);
