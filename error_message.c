b9d4809e1f8c899eac0d72bbf8061498ae0a53d4
Inf1n1ty75 <volkandu75@gmail.com>
2024-01-29 15:55:19 +0100

    norminette

--- error_message.c
+++ error_message.c
@@ -6,13 +6,13 @@
 /*   By: vopekdas <vopekdas@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/22 15:22:07 by vopekdas          #+#    #+#             */
-/*   Updated: 2024/01/29 14:47:47 by vopekdas         ###   ########.fr       */
+/*   Updated: 2024/01/29 15:51:52 by vopekdas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
 
-int	ft_perror_msg()
+int	ft_perror_msg(void)
 {
 	perror("");
 	return (ERROR);
